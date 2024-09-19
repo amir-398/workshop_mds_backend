b17b0e089722433d0174477fdfe2f0f3e8140806
amir-398 <amir.398@hotmail.fr>
2024-09-19 22:46:54 +0200

    Refactoriser les dépendances pour ajouter le module CORS

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,18 @@ import path from "path";
 import mongoose from "mongoose";
 const app = express();
 import cors from "cors";
-mongoose.connect("mongodb://localhost:27017/workshop_mds");
+
+mongoose.connect("mongodb://mongo_db:27017/workshop_mds");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.static(path.join(__dirname, "../public")));
-
+console.log("tes");
 const port = 3001;
 app.use("/categories", require("./routes/categoriesRoute"));
 app.use("/accessoires", require("./routes/accessoireRoute"));
 
+app.use("/products", require("./routes/productRoute"));
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
